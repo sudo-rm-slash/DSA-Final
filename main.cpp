66429110d247bc8a47b1fca45d6db2f0a42c4397
Andy Liu <windows.linux.mac@gmail.com>
2015-06-25 01:45:24 +0800

    Function LUT now separated from dsa namespace.

--- main.cpp
+++ main.cpp
@@ -6,39 +6,33 @@
 
 #include "operations.hpp"
 
-namespace dsa
+/*
+ * Definitions
+ */
+typedef std::map<std::string, std::function<void()> > functions;
+
+
+/*
+ * Global variables.
+ */
+functions lookup_table;
+
+
+/*
+ * Generate function lookup table.
+ */
+#define INSERT(NAME, FUNC) lookup_table.insert(std::pair<std::string, std::function<void()> >(NAME, FUNC))
+void functions_init()
 {
-	/*
-	 * Definitions
-	 */
-	typedef std::map<std::string, std::function<void()> > functions;
-
-
-	/*
-	 * Global variables.
-	 */
-	namespace global
-	{
-		dsa::functions lookup_table;
-	}
-
-
-	/*
-	 * Generate function lookup table.
-	 */
-#define INSERT(NAME, FUNC) dsa::global::lookup_table.insert(std::pair<std::string, std::function<void()> >(NAME, FUNC))
-	void functions_init()
-	{
-		INSERT("login", login);
-		INSERT("create", create);
-		INSERT("delete", remove);
-		INSERT("merge", merge);
-		INSERT("deposit", deposit);
-		INSERT("withdraw", withdraw);
-		INSERT("transfer", transfer);
-		INSERT("find", find);
-		INSERT("search", search);
-	}
+	INSERT("login", 	dsa::login);
+	INSERT("create", 	dsa::create);
+	INSERT("delete", 	dsa::remove);
+	INSERT("merge", 	dsa::merge);
+	INSERT("deposit", 	dsa::deposit);
+	INSERT("withdraw", 	dsa::withdraw);
+	INSERT("transfer",	dsa::transfer);
+	INSERT("find", 		dsa::find);
+	INSERT("search", 	dsa::search);
 }
 
 
@@ -48,15 +42,15 @@ namespace dsa
 int main()
 {
 	// Generate "name to function" lookup table beforehand.
-	dsa::functions_init();
+	functions_init();
 
 	// Start parsing strings
 	std::string operation;
 	while (std::cin >> operation)
 	{
 		// Find the target function.
-		auto search = dsa::global::lookup_table.find(operation);
-		if (search == dsa::global::lookup_table.end())
+		auto search = lookup_table.find(operation);
+		if (search == lookup_table.end())
 		{
 			throw std::invalid_argument("Invalid operation \"" + operation + "\".\n");
 		}
