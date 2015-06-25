ec4cc5a6b6402adca1fed93bd85cb22d2fb4f156
Andy Liu <windows.linux.mac@gmail.com>
2015-06-25 16:06:05 +0800

    Implement integer based disjoint sets.

--- disjoint_sets.cpp
+++ disjoint_sets.cpp
@@ -7,30 +7,81 @@ namespace dsa
 	template <class T>
 	disjoint_sets<T>::disjoint_sets()
 	{
-
-	}
-
-	template <class T>
-	disjoint_sets<T>::disjoint_sets(const T& item)
-	{
-
+		// Initialize the vector.
+		sets.clear();
 	}
 
 	template <class T>
 	disjoint_sets<T>::~disjoint_sets()
 	{
-
+		
 	}
 
 	template <class T>
-	disjoint_sets<T> disjoint_sets<T>::find_set(const T& child) const
+	void disjoint_sets<T>::make_set(const T& item)
 	{
 
 	}
 
 	template <class T>
-	void disjoint_sets<T>::link(const T& child1, const T& child2)
+	int disjoint_sets<T>::find_set(int id) const
 	{
+		node* current_node;
 
+		// Find the root element that represents the set which id belongs to.
+		current_node = sets[id];
+		while (current_node->parent != NULL)
+		{
+			current_node = current_node->parent;
+		}
+
+		node* root = current_node;
+
+		// Update the parents of id, make those elements direct children of root.
+		current_node = sets[id];
+		while (current_node != root)
+		{
+			node* next = current_node->parent;
+			current_node->parent = root;
+			current_node = next;
+		}
+
+		return root->id;
+	}
+
+	template <class T>
+	void disjoint_sets<T>::link(int id1, int id2)
+	{
+		// Already the same union.
+		if (id1 == id2)
+		{
+			return;
+		}
+
+		node* set1 = nodes[id1];
+		node* set2 = nodes[id2];
+
+		// Determine which node represent the higher rank.
+		if (set1->rank > set2->rank)
+		{
+			set2->parent = set1;
+		}
+		else if (set1->rank < set2->rank)
+		{
+			set1->parent = set2;
+		}
+		else
+		{
+			set2->parent = set1;
+
+			// Increase the rank of a set to break the tie.
+			++set1->rank;
+		}
+	}
+
+	template <class T>
+	int disjoint_sets<T>::get_sets()
+	{
+		return this->sets.size();
 	}
 }
--- disjoint_sets.hpp
+++ disjoint_sets.hpp
@@ -8,15 +8,44 @@ namespace dsa
 	class disjoint_sets
 	{
 	private:
-		std::vector<T> forest;
-		
+		struct node
+		{
+			node* parent;	// The parent node of this node.
+			int id;			// The index of this node.
+			int rank;		// Roughly represent the max height of the node in its subtree.
+			T data;			// The data this node carries.
+		};
+		std::vector<node*> sets;
+
 	public:
 		disjoint_sets();
-		disjoint_sets(const T& item);
-		~disjoint_sets();
+		virtual ~disjoint_sets();
 
-		disjoint_sets<T> find_set(const T& child) const;
+		void make_set(const T& item);
+
+		/*
+		 * @brief Find the set child belongs to.
+		 *
+		 * @ param child The child to look up for.
+		 * @ return The set ID of the root node.
+		 */
+		int find_set(const T& child) const;
+
+		/*
+		 * @brief Merge the sets that child1 and child2 belogns to.
+		 *
+		 * @param child1 The first child.
+		 * @param child2 The second child.
+		 * @return Void.
+		 */
 		void link(const T& child1, const T& child2);
+
+		/*
+		 * @brief Get the number of sets this container currently holds.
+		 *
+		 * @return The sets count.
+		 */
+		int get_sets();
 	};
 }
 
