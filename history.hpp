664e40c1b97d5504b29651b7cc42b8525423b749
Andy Liu <windows.linux.mac@gmail.com>
2015-06-26 11:49:00 +0800

    Initialize history source files.

---
+++ history.hpp
@@ -0,0 +1,19 @@
+#ifndef __HISTORY_HPP__
+#define __HISTORY_HPP__
+
+#include <vecotor>		// std::vector
+
+#include "account.hpp"
+
+namespace dsa
+{
+	class history
+	{
+	private:
+		std::vector<account> container;
+
+	public:
+	};
+}
+
+#endif
