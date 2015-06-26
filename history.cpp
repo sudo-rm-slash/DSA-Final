6717d9b7cf2ee7aed5cc7ef45b55450785f5b38c
Andy Liu <windows.linux.mac@gmail.com>
2015-06-26 14:56:52 +0800

    Add subscripting overload to output the targeted entry.

--- history.cpp
+++ history.cpp
@@ -4,12 +4,25 @@ dsa::history::history()
 {
 	// Initialize the vector.
 	this->container.clear();
+
+	// Reset the base ID flag.
+	this->base_id = -1;
+
+	// Set the default output method as std::cout.
+	this->output = std::cout;
 }
 
 dsa::history::~history()
 {
-	// Wipe out the private variable.
+	// Delete all the entries in this container.
+	for (int i = this->container.size() - 1; i >= 0; i--)
+	{
+		delete this->container[i];
+	}
+
+	// Wipe out all the private variables.
 	this->container.clear();
+	this->base_id = -1;
 }
 
 int dsa::history::insert(const int& from_id, const int& to_id, unsigned int value)
@@ -25,4 +38,64 @@ int dsa::history::insert(const int& from_id, const int& to_id, unsigned int valu
 	this->container[index]->value = value;
 
 	return index;
+}
+
+void set_criteria(const int& base_id)
+{
+	this->base_id = base_id;
+}
+
+void set_output_stream(const ostream& stream)
+{
+	this->output = stream;
+}
+
+void dsa::history::operator[](const int& associated_id)
+{
+	// First round of the search.
+	auto it = std::find_if(std::begin(this->container), std::end(this->container), [](entry e)
+	{
+		if ((e.from == this->base_id) && (e.to == associated_id))
+		{
+			output << "To " << e.value << std::endl;
+			return true;
+		}
+		else if ((e.from == associated_id) && (e.to == this->base_id))
+		{
+			output << "From " << e.value << std::endl;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	});
+
+	// Quit when no record exist, which means the iterator is pointed at the end.
+	if (it == std::end(this->container))
+	{
+		output << "no output\n";
+		return;
+	}
+
+	while (it != std::end(this->container))
+	{
+		it = std::find_if(std::next(it), std::end(this->container), [](entry e)
+		{
+			if ((e.from == this->base_id) && (e.to == associated_id))
+			{
+				output << "To " << e.value << std::endl;
+				return true;
+			}
+			else if ((e.from == associated_id) && (e.to == this->base_id))
+			{
+				output << "From " << e.value << std::endl;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		});
+	}
 }
--- history.hpp
+++ history.hpp
@@ -2,6 +2,8 @@
 #define __HISTORY_HPP__
 
 #include <vecotor>		// std::vector
+#include <algorithm>	// std::find_if
+#include <ostream>		// std::ostream, std::cout
 
 #include "account.hpp"
 
@@ -16,8 +18,21 @@ namespace dsa
 			int to;
 			unsigned int value;
 		};
-
 		std::vector<entry> container;
+
+		int base_id;		// Normally, it's the last successful login ID.
+		ostream output;		// Default output method.
+
+		/*
+		 * DEPRECATED
+		 *
+		 * @brief Inline method for std::find_if lambda operation.
+		 *
+		 * @param from_id From whom...
+		 * @param to_id ...to whom.
+		 * @return Whether the condition matches the setting.
+		 */
+		inline bool criteria_match(const int& from_id, const int& to_id);
 
 	public:
 		history();
@@ -32,6 +47,31 @@ namespace dsa
 		 * @return The ID of current entry.
 		 */
 		int insert(const int& from_id, const int& to_id, unsigned int value);
+
+		/*
+		 * @brief Set the base ID variable, normally, it denotes the last successful login ID.
+		 *
+		 * @param base_id The base ID.
+		 * @return Void.
+		 */
+		void set_criteria(const int& base_id);
+
+		/*
+		 * @brief Override the default output stream (std::cout).
+		 *
+		 * @param stream Desired ostream object.
+		 * @return Void.
+		 */
+		void set_output_stream(const ostream& stream);
+
+		/*
+		 * @brief Subscripting operator overload to easily print out A/B related entries,
+		 *		  Use set_critera before using this function.
+		 *
+		 * @param associated_id The associated ID.
+		 * @return Void.
+		 */
+		void operator[](const int& associated_id);
 	};
 }
 
