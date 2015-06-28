7b50c509a25277dc0a17f1564ad57aa41f9d048c
IthacaCow <cba840910@gmail.com>
2015-06-28 17:03:55 +0800

    fix template compile error and build better test case

--- recommendation.hpp
+++ recommendation.hpp
@@ -6,13 +6,11 @@
 #include <iostream>
 #include <string>
 
-#ifndef DEBUG
 #define RECOMMENDATION_NUMBER 10
-#endif
+#define CANDIDATES_SIZE 62
 
 typedef std::pair<char,char> bound_t;
 
-#define CANDIDATES_SIZE 62
 namespace dsa 
 {
 	template<class T>
@@ -22,19 +20,12 @@ namespace dsa
 	private:
 		static const char candidates_characters[];
 		std::vector<char*> recommendations;
-		char* original_text;
+		const char* original_text;
 		T& container; 
-#ifdef DEBUG
-		int RECOMMENDATION_NUMBER;
-#endif
 
 	public:
 
-#ifdef DEBUG
-		recommendation(int recommendation_number, T& _container):container(_container), RECOMMENDATION_NUMBER(recommendation_number){}
-#else
 		recommendation(T& _container):container(_container){}
-#endif
 
 		void recommend(const char* _original_text);
 		void print_recommendation();
@@ -45,10 +36,11 @@ namespace dsa
 		bool enumerate_single_character( char* candidate_string, int position, int length, char upperbound = '\0');
 
 		bool enumerate_single_character( char* candidate_string, int position, int length, bound_t bounds );
-		bool enumerate_double_character( char* candidate_string, int length, std::pair<int,int> positions, std::pair<bound_t,bound_t>& bounds_pair );
+		bool enumerate_double_character( char* candidate_string, int length, std::pair<int,int> positions, std::pair<bound_t,bound_t>&& bounds_pair );
 
 	};
 
 }
 
+#include "recommendation.tpp"
 #endif // end of __RECOMMENDATION_HPP__
--- test_recommendation/test_recommendation.cpp
+++ test_recommendation/test_recommendation.cpp
@@ -1,7 +1,12 @@
 #include <unordered_map>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include "../recommendation.hpp"
 
+#define TEST_LENGTH 8
+
+const char candidates_characters[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 class test_hashmap
 {
 	private:
@@ -11,46 +16,90 @@ class test_hashmap
 		{
 			std::cout << "Test hashmap constructed\n";
 			char test_case[] = "abc";
-			for( char i = 'a'; i < 'a' + scale; ++i )
+			for( char i = 0; i < scale; ++i )
 			{
-				test_case[0] = i;
-				for( char j = 'a'; j < 'a' + scale; ++j )
+				test_case[0] = candidates_characters[i];
+				for( char j = 0; j < scale; ++j )
 				{
-					test_case[1] = j;
-					for( char k = 'a'; k < 'a' + scale; ++k )
+					test_case[1] = candidates_characters[j];
+					for( char k = 0; k < scale; ++k )
 					{
-						test_case[2] = k;
+						test_case[2] = candidates_characters[k];
 						map.emplace( test_case, test_case );
 						std::cout << "Puting " << test_case << std::endl;
 					}
 				}
 			}
 		}
+
+		void insert_hashmap( char* test_case, int scale, int dept )
+		{
+			if( dept < 0 )
+			{
+				if( rand() % 10 > 3 )
+				{
+					map.emplace( test_case, test_case );
+					std::cout << "Putting " <<test_case << std::endl;
+				}
+			}	
+			else
+			{
+				for( char i = 0; i < scale; ++i )
+					test_case[dept] = candidates_characters[i];
+				insert_hashmap( scale, dept-1 );
+			}
+
+		}
 		bool exist( std::string key )
 		{
+			// if( map.find( key ) != map.end() )
+				// std::cout << "Exist: " << key << std::endl;
+			// else
+				// std::cout << "Not exist: " << key << std::endl;
+
 			return map.find( key ) != map.end();
 		}
 
 
 };
+
+int test(const char* testcase)
+{
+	recommendation_test.recommend("ade");
+	std::cout << "Recommendation for " << test_case << ": ";
+	recommendation_test.print_recommendation();
+	recommendation_test.flush();
+}
 int main(int argc, char *argv[])
 {
 
 	std::cout << " Start testing recommendation\n";
 	test_hashmap hashmap(5);
-	dsa::recommendation<test_hashmap> recommendation(20,hashmap);
 
-	recommendation.recommend("ade");
-	recommendation.print_recommendation();
-	recommendation.flush();
+	srand (time(NULL));
+	char* test_case = new char[ TEST_LENGTH+1 ];
+	test_case[ TEST_LENGTH ] = '\0';
+	insert_hashmap( 5, TEST_LENGTH );
+
+	dsa::recommendation<test_hashmap> recommendation_test(hashmap);
+
+
+	void insert_hashmap( char* test_case, int scale, int dept )
+	{
+		if( dept < 0 )
+		{
+			map.emplace( test_case, test_case );
+			std::cout << "Putting " <<test_case << std::endl;
+		}	
+		else
+		{
+			for( char i = 0; i < scale; ++i )
+				test_case[dept] = candidates_characters[i];
+			insert_hashmap( scale, dept-1 );
+		}
+
+	}
 	 
-	recommendation.recommend("ddd");
-	recommendation.print_recommendation();
-	recommendation.flush();
-
-	recommendation.recommend("abc");
-	recommendation.print_recommendation();
-	recommendation.flush();
 
 	return 0;
 }
