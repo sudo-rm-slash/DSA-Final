a58edb5e479e70929584b611891e00d82a31f6a4
Andy Liu <windows.linux.mac@gmail.com>
2015-06-28 16:53:28 +0800

    Implement create().

--- src/operations.cpp
+++ src/operations.cpp
@@ -5,12 +5,16 @@ void dsa::login()
 	std::cin >> username >> password;
 
 	// Check the existance of the username.
-	last_login_id = lookup_table.find_specific(username);
-	if (last_login_id == -1)
+	if (finder.exists(username))
+	{
+		last_login_id = finder.find_specific(username);
+	}
+	else
 	{
 		std::cout << "ID " << username << " not found" << std::endl;
 		return;
 	}
+
 	// TODO:
 	// Wil the user the master owner after the lookup?
 	// Do I need to reverse lookup for it?
@@ -34,19 +38,18 @@ void dsa::create()
 	std::cin >> username >> password;
 
 	// Check the existance of the username.
-	int id = lookup_table.find_specific(username);
-	if (id != -1)
+	if (finder.exists(username))
 	{
 		std::cout << "ID " << username << " exists,";
 
 		// Get suggestions and print them.
-		lookup_table.suggest_nonexists(username, suggestions);
+		finder.suggest_nonexists(username, suggestions);
 		if (suggestions.size() > 0)
 		{
 			std::cout << suggestions[0];
-			for (int i = 1; i < suggestions.size(); i++)
+			for (auto itr = std::begin(suggestions); itr != std::end(suggestions); ++itr)
 			{
-				std::cout << ',' << suggestions[i];
+				std::cout << ',' << *itr;
 			}
 		}
 		std::cout << std::endl;
@@ -55,13 +58,13 @@ void dsa::create()
 	}
 
 	// Create a new account in the storage.
-	int index = accounts.new_account(username, password);
+	unsigned int index = accounts.new_account(username, password);
 
 	// Add the relationship of this account.
-	id = relationship.add_user(index);
+	unsigned int id = ownerships.add_user(index);
 
 	// Store the account into the lookup table.
-	lookup_table.insert(username, id);
+	finder.insert(username, id);
 
 	std::cout << "success" << std::endl;
 }
