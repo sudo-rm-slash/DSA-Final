e42d5482b8bdc12e3881582eb3ef36ef3f9f33c3
Andy Liu <windows.linux.mac@gmail.com>
2015-06-28 16:30:14 +0800

    Implement create().

--- src/operations.cpp
+++ src/operations.cpp
@@ -6,7 +6,7 @@ void dsa::login()
 
 	// Check the existance of the username.
 	last_login_id = lookup_table.find_specific(username);
-	if(last_login_id == -1)
+	if (last_login_id == -1)
 	{
 		std::cout << "ID " << username << " not found" << std::endl;
 		return;
@@ -14,9 +14,9 @@ void dsa::login()
 	// TODO:
 	// Wil the user the master owner after the lookup?
 	// Do I need to reverse lookup for it?
-	
+
 	// Authenticate the password.
-	if(accounts[last_login_id].authenticate(password))
+	if (accounts[last_login_id].authenticate(password))
 	{
 		std::cout << "success" << std::endl;
 	}
@@ -31,7 +31,39 @@ void dsa::login()
 
 void dsa::create()
 {
+	std::cin >> username >> password;
 
+	// Check the existance of the username.
+	int id = lookup_table.find_specific(username);
+	if (id != -1)
+	{
+		std::cout << "ID " << username << " exists,";
+
+		// Get suggestions and print them.
+		lookup_table.suggest_nonexists(username, suggestions);
+		if (suggestions.size() > 0)
+		{
+			std::cout << suggestions[0];
+			for (int i = 1; i < suggestions.size(); i++)
+			{
+				std::cout << ',' << suggestions[i];
+			}
+		}
+		std::cout << std::endl;
+
+		return;
+	}
+
+	// Create a new account in the storage.
+	int index = accounts.new_account(username, password);
+
+	// Add the relationship of this account.
+	id = relationship.add_user(index);
+
+	// Store the account into the lookup table.
+	lookup_table.insert(username, id);
+
+	std::cout << "success" << std::endl;
 }
 
 void dsa::delete_()
--- src/operations.hpp
+++ src/operations.hpp
@@ -6,22 +6,25 @@
 #include "lookup_table.hpp"		// dsa::lookup_table
 #include "history.hpp"			// dsa::history
 
+#include <string>				// std::string
+#include <vector>				// std::vector
 
 namespace dsa
 {
 	/**
 	 * Global variables in namespace "dsa".
 	 */
-	dsa::storage accounts;				// The accounts themselves.
-	dsa::relationships ownerships;		// The ownership between accounts.
-	dsa::lookup_table finder;			// Support functions to find accounts.
+	dsa::storage accounts;					// The accounts themselves.
+	dsa::relationships ownerships;			// The ownership between accounts.
+	dsa::lookup_table finder;				// Support functions to find accounts.
 
-	dsa::history transaction_history;	// The grand transaction history.
-										// History is dependent on accounts and ownerships.
+	dsa::history transaction_history;		// The grand transaction history.
+											// History is dependent on accounts and ownerships.
 	
-	std::string username, password;		// Username and password for reuse. 
-	unsigned int last_login_id;			// Last login ID (relationship ID).
+	std::string username, password;			// Username and password for reuse. 
+	unsigned int last_login_id;				// Last login ID (relationship ID).
 
+	std::vector<std::string> suggestions;	// Container to store all the suggestions.
 
 	//
 	// @brief Login to a specific account.
